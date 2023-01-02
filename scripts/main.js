4bc0ae464f93c843f6bb84d337f03faea7a406bc
Rostyslav <rostik.klym@gmail.com>
2023-01-02 17:17:47 +0200

    loader script done

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,20 @@
 
 function setupMenu(menuElement) {
   for (let element of menuElement.children) {
-    // Adding the 'setActive' handler for each 'menu' element on click
     element.addEventListener("click", function setActive() {
-      // Removing the 'active' class for each 'menu' element
       for (let element of this.parentElement.children) {
         element.classList.remove("active");
       }
-      // Setting the 'active' class for current (clicked) element
+
       this.classList.add("active");
 
       for (let element of menuElement.parentElement.querySelector(
         ".menu-content"
       ).children) {
         if (this.dataset.itemName.includes(element.dataset.itemContent)) {
-          // Setting element to visible
           element.classList.remove("hidden");
           element.classList.add("visible");
         } else {
-          // Setting element to hidden
           element.classList.remove("visible");
           element.classList.add("hidden");
         }
@@ -28,4 +24,42 @@ function setupMenu(menuElement) {
   }
 }
 
+function setupLoading(loadElement, loadStep, loadMax) {
+  loadElement.addEventListener("click", function loadElements(event) {
+    function showLoader() {
+      this.innerHTML =
+        '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+    }
+
+    setTimeout(() => {
+      const menuContent = this.parentElement.querySelector(".menu-content");
+
+      for (let i = 0; i < loadStep; i++) {
+        let newElement =
+          menuContent.children[
+            Math.floor(Math.random() * (loadStep - 1))
+          ].cloneNode(true);
+
+        menuContent.lastElementChild.after(newElement);
+      }
+
+      if (menuContent.childElementCount >= loadMax) {
+        this.remove();
+      }
+    }, 1000);
+
+    event.preventDefault();
+  });
+}
+
 document.querySelectorAll(".menu").forEach((element) => setupMenu(element));
+
+document
+  .querySelectorAll(".btn-load")
+  .forEach((element) =>
+    setupLoading(
+      element,
+      element.parentElement.querySelector(".menu-content").childElementCount,
+      element.parentElement.querySelector(".menu-content").childElementCount * 3
+    )
+  );
